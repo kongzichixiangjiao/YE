22591e596c9a88a37eca27674d530b29150d5f4c
kongzichixiangjiao <58895347@qq.com>
2017-08-08 17:52:45 +0800

    cocoapods
    第三方导入

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+inhibit_all_warnings!
+use_frameworks!
+
+target 'YE’ do 
+pod 'HandyJSON', '~> 1.7.2'
+pod 'ReactiveSwift'
+
+
+pod 'HyphenateLite'
+pod 'EaseUILite', :git =>'https://github.com/easemob/easeui-ios-hyphenate-cocoapods.git', :tag => '3.3.2'
+
+end 
---
+++ Pods/Target Support Files/EaseUILite/EaseUILite-umbrella.h
@@ -0,0 +1,80 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "EMCDDeviceManagerDelegate.h"
+#import "EMCDDeviceManagerProximitySensorDelegate.h"
+#import "DemoErrorCode.h"
+#import "EMAudioPlayerUtil.h"
+#import "EMAudioRecorderUtil.h"
+#import "EMCDDeviceManager+Media.h"
+#import "EMCDDeviceManager+Microphone.h"
+#import "EMCDDeviceManager+ProximitySensor.h"
+#import "EMCDDeviceManager+Remind.h"
+#import "EMCDDeviceManager.h"
+#import "EMCDDeviceManagerBase.h"
+#import "EMVoiceConverter.h"
+#import "EaseUI.h"
+#import "EaseSDKHelper.h"
+#import "EaseConversationModel.h"
+#import "EaseMessageModel.h"
+#import "EaseUserModel.h"
+#import "IConversationModel.h"
+#import "IMessageModel.h"
+#import "IModelCell.h"
+#import "IModelChatCell.h"
+#import "IUserModel.h"
+#import "NSDate+Category.h"
+#import "NSDateFormatter+Category.h"
+#import "NSString+Valid.h"
+#import "UIImage+Resource.h"
+#import "UIViewController+DismissKeyboard.h"
+#import "UIViewController+HUD.h"
+#import "EaseChineseToPinyin.h"
+#import "EaseConvertToCommonEmoticonsHelper.h"
+#import "EaseLocalDefine.h"
+#import "EaseMessageReadManager.h"
+#import "EaseEmoji.h"
+#import "EaseEmojiEmoticons.h"
+#import "EaseEmotionEscape.h"
+#import "EaseEmotionManager.h"
+#import "EaseConversationListViewController.h"
+#import "EaseLocationViewController.h"
+#import "EaseMessageViewController.h"
+#import "EaseRefreshTableViewController.h"
+#import "EaseUsersListViewController.h"
+#import "EaseViewController.h"
+#import "EaseUserCell.h"
+#import "EaseBaseMessageCell.h"
+#import "EaseCustomMessageCell.h"
+#import "EaseMessageCell.h"
+#import "EaseMessageTimeCell.h"
+#import "EaseBubbleView+File.h"
+#import "EaseBubbleView+Gif.h"
+#import "EaseBubbleView+Image.h"
+#import "EaseBubbleView+Location.h"
+#import "EaseBubbleView+Text.h"
+#import "EaseBubbleView+Video.h"
+#import "EaseBubbleView+Voice.h"
+#import "EaseBubbleView.h"
+#import "EaseChatToolbar.h"
+#import "EaseChatToolbarItem.h"
+#import "EaseImageView.h"
+#import "EaseTextView.h"
+#import "EaseFaceView.h"
+#import "EaseFacialView.h"
+#import "EaseChatBarMoreView.h"
+#import "EaseRecordView.h"
+#import "EaseConversationCell.h"
+
+FOUNDATION_EXPORT double EaseUILiteVersionNumber;
+FOUNDATION_EXPORT const unsigned char EaseUILiteVersionString[];
+
