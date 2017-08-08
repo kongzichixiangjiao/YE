d299e396ad8e349e67213d199e9ba87a0423b39f
kongzichixiangjiao <58895347@qq.com>
2017-08-08 17:39:15 +0800

    Initial Commit

---
+++ YE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YE
+//
+//  Created by 侯佳男 on 2017/8/8.
+//  Copyright © 2017年 侯佳男. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
